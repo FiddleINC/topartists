6caeb49a1b1e89c50928863f5d17fce9f6726a43
fiddleinc <prachurrio@gmail.com>
2019-06-13 16:01:28 +0530

    Updates

---
+++ precache-manifest.b4c6bd95b006b21a1e5ba1fd6fddc4cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c4ffaf287ad60f3f220b",
+    "url": "/topartists/static/js/runtime~main.8c030bde.js"
+  },
+  {
+    "revision": "06b4d66661d3f0019e96",
+    "url": "/topartists/static/js/main.e4c3365b.chunk.js"
+  },
+  {
+    "revision": "3ceedd31f00079ec580a",
+    "url": "/topartists/static/js/2.396d74fe.chunk.js"
+  },
+  {
+    "revision": "06b4d66661d3f0019e96",
+    "url": "/topartists/static/css/main.9adca9a4.chunk.css"
+  },
+  {
+    "revision": "08d189f8ec7c565233a318d2a7a0f9ef",
+    "url": "/topartists/index.html"
+  }
+];
