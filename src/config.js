082ee46c53aec51ee318125a4df310405458814b
fiddleinc <prachurrio@gmail.com>
2019-06-13 15:43:29 +0530

    changed redirect uri

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ export const authEndpoint = "https://accounts.spotify.com/authorize";
 
 // Replace with your app's client ID, redirect URI and desired scopes
 export const clientId = "3ffc762137bb4446a79df7e9cde4a1e6";
-export const redirectUri = "http://localhost:3000/redirect";
+export const redirectUri = "https://topartists.herokuapp.com/artists#";
 export const scopes = [
   "user-top-read",
   "user-read-currently-playing",
