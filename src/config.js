180968401182f9a1adb74720e1eb4f6777195226
fiddleinc <prachurrio@gmail.com>
2019-06-13 15:37:27 +0530

    Adding Config File

---
+++ src/config.js
@@ -0,0 +1,10 @@
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+
+// Replace with your app's client ID, redirect URI and desired scopes
+export const clientId = "3ffc762137bb4446a79df7e9cde4a1e6";
+export const redirectUri = "http://localhost:3000/redirect";
+export const scopes = [
+  "user-top-read",
+  "user-read-currently-playing",
+  "user-read-playback-state"
+];
